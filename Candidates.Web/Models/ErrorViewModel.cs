08c149e9803c1fb66cf0c7e215d9de7f000f9a7f
Juanma <jmoliveras@kiteris.com>
2022-10-13 15:43:58 +0200

    Initial commit

---
+++ Candidates.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Candidates.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
